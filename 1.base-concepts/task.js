2342792d88c04d48247bbaaff63bd062d1f56d62
Игорь Харченко <igorharcenko@MacBook-Air-Igor.local>
2023-01-28 17:37:24 +0500

    Первое задание

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -2,22 +2,22 @@
 function solveEquation(a, b, c) {
   let arr = [];
   let rootFirst;
-  let rootSecond;
-  let discriminantFormula = Math.pow(b, 2) - 4 * a * c;
+  // let rootSecond;
+  let d = Math.pow(b, 2) - 4 * a * c;
 
-  if (discriminantFormula < 0) {
-    console.log('Корней нет')
-  } else if (discriminantFormula === 0) {
-    rootFirst = -b / (2 * a);
-    arr.push(rootFirst);
-    console.log(arr);
-  } else {
-    rootFirst = (-b + Math.sqrt(d) ) / (2 * a);
-    rootSecond = (-b - Math.sqrt(d) ) / (2 * a);
-    arr.push(rootFirst);
-    arr.push(rootSecond);
-    console.log('Первый корень = ' + arr[0] + 'Второй корень = ' + arr[1]);
-  }
+    if (d === 0) {
+      rootFirst = -b / (2 * a);
+      arr.push(rootFirst);
+      console.log(arr);
+    } else if (d > 0) {
+      rootFirst = (-b + Math.sqrt(d) ) / (2 * a);
+      let rootSecond = (-b - Math.sqrt(d) ) / (2 * a);
+      arr.push(rootFirst);
+      arr.push(rootSecond);
+      console.log('Первый корень = ' + arr[0] + 'Второй корень = ' + arr[1]);
+    } else {
+      console.log('Корней нет');
+    }
 
   return arr;
 }
@@ -37,9 +37,9 @@ function calculateTotalMortgage(percent, contribution, amount, countMonths) {
       return false;
     }
 
-    percent = (percent / 100) / 12;
-    bodyCredit = amount / contribution;
-    monthPayment = bodyCredit * (percent + (percent /  ((Math.pow((1 + percent), countMonths)) - 1)));
+    percent = percent / 100 / 12;
+    bodyCredit = amount - contribution;
+    monthPayment = (bodyCredit * percent + (percent /  ((Math.pow((1 + percent), countMonths)) - 1)));
     totalAmount = parseFloat((monthPayment * countMonths).toFixed(2));
 
     return totalAmount;
