67efb0d2220d7d57383ee13fc8a7e7716333f860
Игорь Харченко <igorharcenko@MacBook-Air-Igor.local>
2023-03-04 19:13:22 +0500

    Шестое задание

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,61 @@
-﻿
+﻿function parseCount(quantityOfGoods) {
+    const result = Number.parseFloat(quantityOfGoods);
+    if (!result) {
+      throw new Error('Невалидное значение');
+    }
+    return result;
+  }
+
+  function validateCount(count) {
+    try {
+        return parseCount(count);
+    } catch(error) {
+        return error;
+    }
+}
+
+//Задание 2
+
+class Triangle {
+    constructor(a, b, c) {
+        if ((a + b) < c || (a + c) < b || (b + c) < a) {
+            throw new Error("Треугольник с такими сторонами не существует");
+        }
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    get perimeter() {
+        const perimeter = this.a + this.b + this.c;
+        return perimeter;
+    }
+
+    get arrea() {
+        const semiPerimeter = this.perimeter / 2;
+        const square = Math.sqrt((semiPerimeter * (semiPerimeter - this.a) * (semiPerimeter - this.b) * (semiPerimeter - this.c))).toFixed(3);
+        return square;
+    }
+}
+
+function getTriangle(a, b, c) {
+
+    try {
+
+        return new Triangle(a, b, c);
+
+    } catch(error) {
+
+        return {
+            get perimeter() {
+                return "Ошибка! Треугольник не существует";
+            },
+            get arrea() {
+                return "Ошибка! Треугольник не существует"; 
+            }
+
+        }
+
+    }
+
+}
